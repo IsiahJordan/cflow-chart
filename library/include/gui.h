276afc407bf5f23169dbba815e31141c2a34656e
Isiah Jordan <dimaunahanisiahjordan@gmail.com>
2026-03-27 19:18:12 +0800

    create gui component

---
+++ library/include/gui.h
@@ -0,0 +1,12 @@
+#ifndef GUI_H
+#define GUI_H
+
+// OBJECTIVE OF THIS FILE
+// 
+// gui.c and gui.h is for handling
+// shape creation, layout creation, 
+// displays and event interface
+
+
+
+#endif
